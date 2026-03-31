dec0b110b1ff79ec03ddae9a1ca55330d98a341a
오현재 <hyunjae1995@gmail.com>
2026-03-31 22:11:10 +0900

    remove: memo form and simplify trip layout

--- app/(main)/trips/[id]/_layout.tsx
+++ app/(main)/trips/[id]/_layout.tsx
@@ -3,7 +3,6 @@ import { Stack } from 'expo-router';
 export default function TripDetailLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
       <Stack.Screen name="edit" />
     </Stack>
   );
