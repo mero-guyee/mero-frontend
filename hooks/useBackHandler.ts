2bf4406e5036027ef6bbbcb5ea9f470c570cd31d
오현재 <hyunjae1995@gmail.com>
2026-04-05 17:44:42 +0900

    feat: Add double-back exit toast and back handling

--- hooks/useBackHandler.ts
+++ hooks/useBackHandler.ts
@@ -1,31 +1,56 @@
 import { router, useFocusEffect, usePathname } from 'expo-router';
-import { useCallback } from 'react';
-import { Alert, BackHandler } from 'react-native';
+import { useCallback, useRef } from 'react';
+import { BackHandler } from 'react-native';
+import Toast from 'react-native-toast-message';
 
 export default function useBackHandler() {
-  const currentPath = usePathname();
+  const path = usePathname();
+  const backPressedOnce = useRef(false);
 
+  const backPressConditions = {
+    nestedPath: path.split('/').length > 2,
+    onTripsMain: path === '/trips',
+  };
+
+  const checkBackpressToExitApp = () => {
+    if (backPressedOnce.current) {
+      BackHandler.exitApp();
+      return true;
+    }
+
+    backPressedOnce.current = true;
+    Toast.show({
+      type: 'handleBackPress',
+      text1: '앱을 종료하시겠습니까?',
+      text2: '뒤로가기를 한 번 더 누르면 종료됩니다.',
+      position: 'bottom',
+      visibilityTime: 2000,
+    });
+
+    setTimeout(() => {
+      backPressedOnce.current = false;
+    }, 2000);
+  };
   useFocusEffect(
     useCallback(() => {
-      if (currentPath !== '/trips') {
-        const onBackPress = () => {
-          router.push('/(main)/trips');
-          return true;
-        };
-
-        const backHandler = BackHandler.addEventListener('hardwareBackPress', onBackPress);
-
-        return () => backHandler.remove();
-      } else {
+      if (backPressConditions.onTripsMain) {
         const sub = BackHandler.addEventListener('hardwareBackPress', () => {
-          Alert.alert('앱 종료', '앱을 종료하시겠습니까?', [
-            { text: '취소', style: 'cancel' },
-            { text: '종료', onPress: () => BackHandler.exitApp() },
-          ]);
+          checkBackpressToExitApp();
           return true;
         });
         return () => sub.remove();
+      } else {
+        if (backPressConditions.nestedPath) {
+          return;
+        }
+
+        const backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
+          router.push('/(main)/trips');
+          return true;
+        });
+
+        return () => backHandler.remove();
       }
-    }, [currentPath])
+    }, [backPressConditions.nestedPath, backPressConditions.onTripsMain])
   );
 }
