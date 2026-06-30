d09593d7cf829ee629da07152f80dec0fac67845
오현재 <hyunjae1995@gmail.com>
2026-06-30 21:27:07 +0900

    fix: expo-sqlite transaction 충돌로인해 병렬 처리 제거

--- utils/photoSync.ts
+++ utils/photoSync.ts
@@ -16,20 +16,19 @@ export async function uploadPhotosAndSync(
     localPhotos
   );
 
-  await Promise.all(
-    uploadedPhotos.map((photo: PhotoResponse, i: number) =>
-      photoRepo.syncUploaded(localPhotos[i].id, {
-        serverId: String(photo.id),
-        s3Url: photo.s3Url,
-        originalFilename: photo.originalFilename,
-        fileSize: photo.fileSize,
-        mimeType: photo.mimeType,
-        width: photo.width,
-        height: photo.height,
-        orderIndex: photo.orderIndex,
-      })
-    )
-  );
+  for (let i = 0; i < uploadedPhotos.length; i++) {
+    const photo = uploadedPhotos[i];
+    await photoRepo.syncUploaded(localPhotos[i].id, {
+      serverId: String(photo.id),
+      s3Url: photo.s3Url,
+      originalFilename: photo.originalFilename,
+      fileSize: photo.fileSize,
+      mimeType: photo.mimeType,
+      width: photo.width,
+      height: photo.height,
+      orderIndex: photo.orderIndex,
+    });
+  }
 
   return uploadedPhotos.map((p: PhotoResponse) => p.s3Url);
 }
